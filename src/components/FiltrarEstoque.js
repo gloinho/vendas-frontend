53b148546b9be4323357f99bc8b789a773734255
Anastacio <gl0inn@hotmail.com>
2022-08-18 15:33:16 -0300

    fix filtro estoque

--- src/components/FiltrarEstoque.js
+++ src/components/FiltrarEstoque.js
@@ -12,21 +12,17 @@ export default function FiltrarEstoque(props){
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
-                [name] : value,
-                'quantidade': checked ? value : ''
+                [name] : name === 'quantidade' ? checked ? value : '': value 
             }
         })
     }
+
     function handleSubmit(event){
         event.preventDefault()
         fetch(`http://127.0.0.1:8000/api/estoques?produto__codigo_de_barras__contains=${formData.produto__codigo_de_barras}&produto__nome__contains=${formData.produto__nome}&quantidade=${formData.quantidade}`)
             .then(response => response.json())
             .then(data => props.setEstoques(data))
-        setFormData({
-            'produto__nome':'',
-            'produto__codigo_de_barras':'',
-            'quantidade':'',
-        })
+
     }
     return <form onSubmit={handleSubmit}className="ms-3 me-3 mt-3">
             <h5>Filtrar por:</h5>
@@ -34,16 +30,16 @@ export default function FiltrarEstoque(props){
                 <div className="input-group-prepend">
                     <span className="input-group-text">Nome do Produto</span>
                 </div>
-                <input onChange={handleChange} name="produto__nome" type="text" className="form-control" aria-label="Nome do Produto" aria-describedby="basic-addon1"></input>
+                <input id ="produto__nome" onChange={handleChange} name="produto__nome" type="text" className="form-control" aria-label="Nome do Produto" aria-describedby="basic-addon1"></input>
              </div>
             <div className="input-group mb-3">
                 <div className="input-group-prepend">
                     <span className="input-group-text">Código de Barras do Produto</span>
                 </div>
-                <input onChange={handleChange} name="produto__codigo_de_barras" type="number" className="form-control" aria-label="Código de Barras" aria-describedby="basic-addon1"></input>
+                <input id ="produto__codigo_de_barras" onChange={handleChange} name="produto__codigo_de_barras" type="number" className="form-control" aria-label="Código de Barras" aria-describedby="basic-addon1"></input>
              </div>
              <div className="form-check form-check-inline">
-                <input onChange={handleChange} name="quantidade" className="form-check-input" type="checkbox" value="0"></input>
+                <input onChange={handleChange} name="quantidade" id="quantidade" className="form-check-input" type="checkbox" value="0"></input>
                 <label className="form-check-label" htmlFor="inlineCheckbox1">Sem Estoque</label>
             </div>
             <button className="btn btn-dark mb-2">Pesquisar</button>
